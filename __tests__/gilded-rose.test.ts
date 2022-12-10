6658a5a1c232f96ffb22d3a3693cf34409817536
Inio Idoate <idoate.127253@e.unavarra.es>
2022-12-10 10:27:21 +0100

    before all initialize items

--- __tests__/gilded-rose.test.ts
+++ __tests__/gilded-rose.test.ts
@@ -1,9 +1,32 @@
 import { Item, GildedRose } from "../app/gilded-rose";
 
 describe("Gilded Rose", function () {
-  it("should foo", function () {
-    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("foo");
+  let allItems = {
+    normalItem: new Item("Normal Item", 10, 20),
+    expiredNormalItem: new Item(" 0 Sell days Item", 0, 20),
+   }
+  beforeEach (() => {
+    allItems = {
+      normalItem: new Item("Normal Item", 10, 20),
+      expiredNormalItem: new Item(" 0 Sell days Item", 0, 20),
+     }
+  })
+  it("should decrease one unit of quality when one day has passed ", function () {
+    const gildedRose = new GildedRose([allItems.normalItem]);
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].quality).toEqual(19);
   });
+  it("should decrease one unit of quality when two days has passed ", function () {
+    const gildedRose = new GildedRose([allItems.normalItem]);
+    let  updatedItems = gildedRose.updateQuality();
+    updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].quality).toEqual(18);
+  });
+
+  it("should decrease two unit of quality when sellin is over ", function () {
+    const gildedRose = new GildedRose([allItems.expiredNormalItem]);
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].quality).toEqual(18);
+  });
+
 });
